345559b3cab7e6f7e7f0e1df4e600f4080a3910e
MarinaTigrova1204 <118852910+MarinaTigrova1204@users.noreply.github.com>
2023-03-06 02:04:26 +0300

    junit тесты

---
+++ ideaProjectMaven/src/main/java/week9/Main.java
@@ -0,0 +1,50 @@
+package week9;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String enteredNumber = getUserNumber();
+        int verificatedNumber = verificatedNumber(enteredNumber);
+        int countedNumber = countedNumber(verificatedNumber);
+        System.out.println(countedNumber);
+    }
+
+    public static String getUserNumber() {
+
+
+        Scanner predel = new Scanner(System.in);
+        System.out.print("Введите любое целое положительное число: ");
+        String userNumberString = predel.nextLine();
+        return userNumberString;
+    }
+
+    public static int verificatedNumber(String userNumberString) {
+        String errorMessage = "Некорректное число";
+
+        try {
+            int userNumber = Integer.parseInt(userNumberString);
+            if (userNumber > 0) {
+                return userNumber;
+            } else {
+                throw new RuntimeException(errorMessage);
+            }
+        } catch (InputMismatchException | NumberFormatException e) {
+            throw new RuntimeException(errorMessage);
+        }
+    }
+
+    public static int countedNumber(int useNumber) {
+        int n = useNumber;
+        int sum = 0;
+
+        for (
+                int i = 1;
+                i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+}
